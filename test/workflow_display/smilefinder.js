9411a3a9b4ae4204fc3b6c63594de27e35516a92
Peter Li <peter@gigasciencejournal.com>
2015-08-19 16:36:33 +0800

    Add align functionality to workflow nodes

--- test/workflow_display/smilefinder.js
+++ test/workflow_display/smilefinder.js
@@ -21,6 +21,15 @@ $(document).ready(function () {
                 'line-color': '#bfbfbf',
                 'target-arrow-color': '#bfbfbf'
             })
+
+            // When selected using a dragged box
+            .selector(":selected")
+            .css({
+                "line-color": "#636363",
+                "target-arrow-color": "#636363",
+                "background-blacken": "0.3"
+            })
+
             .selector('.highlighted')
             .css({
                 'background-color': '#61bffc',
@@ -143,57 +152,287 @@ $(document).ready(function () {
         running = false;
     });
 
+    // Detect select on nodes and edges
+    var selected_elements;
+    cy.on('select', function (evt) {
+        console.log('selected: ' + evt.cyTarget.id());
+        selected_elements = cy.elements(':selected');
+    });
+
+    cy.panningEnabled(true);
+    cy.boxSelectionEnabled(true);
+
     layout.run();
-
-    var $config = $('#config');
-    var $btnParam = $('<div class="param"></div>');
-    $config.append($btnParam);
-
-    var buttons = [
-        {
-            label: 'TB',
-            layoutOpts: {
-                rankDir: 'TB'
-            }
-        },
-
-        {
-          label: 'LR',
-          layoutOpts: {
-            rankDir: 'LR'
-          }
-        }
-    ];
-
-    buttons.forEach(makeButton);
 
     function makeLayout(opts) {
         for (var i in opts) {
             params[i] = opts[i];
         }
-
         return cy.makeLayout(params);
     }
 
-    function makeButton(opts) {
-        var $button = $('<button class="btn btn-default">' + opts.label + '</button>');
+    // Dagre layout top to bottom
+    $('#layout_tb').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
 
-        $btnParam.append($button);
+        layout.stop();
 
-        $button.on('click', function () {
-            layout.stop();
-
-            if (opts.fn) {
-                opts.fn();
+        var opts = {
+            layoutOpts: {
+                name: 'dagre',
+                directed: true,
+                roots: '#a',
+                padding: 10,
+                rankDir: 'TB'
             }
+        };
 
-            layout = makeLayout(opts.layoutOpts);
-            layout.run();
-        });
-    }
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
+
+    // Dagre layout left to right
+    $('#layout_lr').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
+
+        layout.stop();
+
+        var opts = {
+            layoutOpts: {
+                name: 'dagre',
+                directed: true,
+                roots: '#a',
+                padding: 10,
+                rankDir: 'LR'
+            }
+        };
+
+        console.log(opts.layoutOpts);
+
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
+
+    $('#align_left').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
+
+        layout.stop();
+
+        var min_x = 1000;
+        for (var i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                console.log("position x: ", selected_elements[i].position('x'));
+                if (selected_elements[i].position('x') < min_x) {
+                    min_x = selected_elements[i].position('x');
+                }
+            }
+        }
+
+        console.log("min_x: ", min_x);
+        for (i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                selected_elements[i].position('x', min_x);
+                console.log("Node: ", selected_elements[i].position());
+            }
+        }
+
+        var opts = {
+            layoutOpts: {
+                name: 'preset'
+            }
+        };
+
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
+
+    $('#align_middle').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
+
+        layout.stop();
+
+        var max_y = 0;
+        var min_y = 1000;
+        for (var i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                console.log("position y: ", selected_elements[i].position('y'));
+                if (selected_elements[i].position('y') > max_y) {
+                    max_y = selected_elements[i].position('y');
+                }
+                if (selected_elements[i].position('y') < min_y) {
+                    min_y = selected_elements[i].position('y');
+                }
+            }
+        }
+
+        console.log("max_y: ", max_y);
+        console.log("min_y: ", min_y);
+        var diff = (max_y - min_y) / 2;
+        var new_y = min_y + diff
+        console.log("diff: ", diff);
+        console.log("new_y: ", new_y);
+        for (i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+
+                selected_elements[i].position('y', new_y);
+                console.log("Node: ", selected_elements[i].position());
+            }
+        }
+
+        var opts = {
+            layoutOpts: {
+                name: 'preset'
+            }
+        };
+
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
+
+    $('#align_right').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
+
+        layout.stop();
+
+        var max_x = 0;
+        for (var i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                console.log("position x: ", selected_elements[i].position('x'));
+                if (selected_elements[i].position('x') > max_x) {
+                    max_x = selected_elements[i].position('x');
+                }
+            }
+        }
+
+        console.log("max_x: ", max_x);
+        for (i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                selected_elements[i].position('x', max_x);
+                console.log("Node: ", selected_elements[i].position());
+            }
+        }
+
+        var opts = {
+            layoutOpts: {
+                name: 'preset'
+            }
+        };
+
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
+
+    $('#align_top').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
+
+        layout.stop();
+
+        var max_y = 1000;
+        for (var i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                console.log("position y: ", selected_elements[i].position('y'));
+                if (selected_elements[i].position('y') < max_y) {
+                    max_y = selected_elements[i].position('y');
+                }
+            }
+        }
+
+        console.log("max_y: ", max_y);
+        for (i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                selected_elements[i].position('y', max_y);
+                console.log("Node: ", selected_elements[i].position());
+            }
+        }
+
+        var opts = {
+            layoutOpts: {
+                name: 'preset'
+            }
+        };
+
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
+
+    $('#align_centre').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
+
+        layout.stop();
+
+        var max_x = 0;
+        var min_x = 1000;
+        for (var i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                console.log("position x: ", selected_elements[i].position('x'));
+                if (selected_elements[i].position('x') > max_x) {
+                    max_x = selected_elements[i].position('x');
+                }
+                if (selected_elements[i].position('x') < min_x) {
+                    min_x = selected_elements[i].position('x');
+                }
+            }
+        }
+
+        console.log("max_x: ", max_x);
+        console.log("min_x: ", min_x);
+        var diff = (max_x - min_x) / 2;
+        var new_x = min_x + diff
+        console.log("diff: ", diff);
+        console.log("new_x: ", new_x);
+        for (i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+
+                selected_elements[i].position('x', new_x);
+                console.log("Node: ", selected_elements[i].position());
+            }
+        }
+
+        var opts = {
+            layoutOpts: {
+                name: 'preset'
+            }
+        };
+
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
+
+    $('#align_bottom').click(function (e) {
+        e.preventDefault();// prevent the default anchor functionality
+
+        layout.stop();
+
+        var min_y = 0;
+        for (var i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                console.log("position y: ", selected_elements[i].position('y'));
+                if (selected_elements[i].position('y') > min_y) {
+                    min_y = selected_elements[i].position('y');
+                }
+            }
+        }
+
+        console.log("min_y: ", min_y);
+        for (i = 0; i < selected_elements.length; i++) {
+            if (selected_elements[i].isNode()) {
+                selected_elements[i].position('y', min_y);
+                console.log("Node: ", selected_elements[i].position());
+            }
+        }
+
+        var opts = {
+            layoutOpts: {
+                name: 'preset'
+            }
+        };
+
+        layout = makeLayout(opts.layoutOpts);
+        layout.run();
+    });
 
 }); // on dom ready
 
-$(function() {
-  FastClick.attach( document.body );
-});
+//$(function() {
+//  FastClick.attach( document.body );
+//});
